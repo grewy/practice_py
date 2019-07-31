fbe3b83fae0a5cd772aee4a451b9eb4569b2d387
grewy <mgatuiet@gmail.com>
2019-07-31 21:37:17 +0530

    Add basic race condition example

--- race_condition_basic.py
+++ race_condition_basic.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python
+
+import threading
+
+x = 0
+
+def increment():
+    """..."""
+    global x
+    x += 1
+
+def thread_task(lock):
+    """..."""
+    for _ in xrange(10000):
+        lock.acquire()
+        increment()
+        lock.release()
+
+def main_task():
+    """..."""
+    global x
+    x = 0
+
+    lock = threading.Lock()
+
+    t1 = threading.Thread(target=thread_task, args=(lock,))
+    t2 = threading.Thread(target=thread_task, args=(lock,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+if __name__ == "__main__":
+    for idx in xrange(1, 11):
+        main_task()
+        print "iteration {0}: {1}".format(idx, x)
