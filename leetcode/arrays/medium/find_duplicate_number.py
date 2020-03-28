31a2a35bbd57b8d6289eda0e1d8b4a6c9a4dfcad
grewy <mgatuiet@gmail.com>
2020-03-28 20:29:48 +0530

    find duplicate number - floyd cycle, hare, tortoise

---
+++ leetcode/arrays/medium/find_duplicate_number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        p1 = nums[0]
+        p2 = slow
+
+        while p1 != p2:
+            p1 = nums[p1]
+            p2 = nums[p2]
+
+        return p1
