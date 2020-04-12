578808b5f8437adff593257893a228ad2eb67715
grewy <mgatuiet@gmail.com>
2020-04-13 01:00:19 +0530

    cycle begin

---
+++ leetcode/misc/linked_list/begin_cycle_ll_2.py
@@ -0,0 +1,34 @@
+"""
+from discuss
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        while fast is not None:
+            fast = fast.next
+            if fast is None:
+                return None
+            fast = fast.next
+            slow = slow.next
+            if slow == fast:
+                break
+
+        if fast is None:
+            return None
+
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
