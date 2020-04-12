c1eb4d5d975f1b6c4ee918e6d18048bfe2b5a558
grewy <mgatuiet@gmail.com>
2020-04-12 23:23:53 +0530

    alternate sol

--- leetcode/misc/linked_list/remove_nth_node_from_end_ll.py
+++ leetcode/misc/linked_list/remove_nth_node_from_end_ll.py
@@ -31,3 +31,18 @@ class Solution(object):
             head = curr.next
 
         return head
+
+"""
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/discuss/9032/Python-concise-one-pass-solution-with-dummy-head.
+"""
+def removeNthFromEnd(self, head, n):
+    dummy = ListNode(0)
+    dummy.next = head
+    fast = slow = dummy
+    for _ in xrange(n):
+        fast = fast.next
+    while fast and fast.next:
+        fast = fast.next
+        slow = slow.next
+    slow.next = slow.next.next
+    return dummy.next
