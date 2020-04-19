33e7659766cdcd70b04d692892ef3c59fdf8e9a7
grewy <mgatuiet@gmail.com>
2020-04-19 22:17:06 +0530

    practice

---
+++ leetcode/data_structures/stack/clone_graph_itr.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = []):
+        self.val = val
+        self.neighbors = neighbors
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return
+
+        nodeCopy = Node(node.val)
+        dic = {node: nodeCopy}
+
+        self.dfs(node, dic)
+        return nodeCopy
+
+    def dfs(self, node, dic):
+        for neighbor in node.neighbors:
+            if neighbor not in dic:
+                neighborCopy = Node(neighbor.val)
+                dic[neighbor] = neighborCopy
+                dic[node].neighbors.append(neighborCopy)
+                self.dfs(neighbor, dic)
+            else:
+                dic[node].neighbors.append(dic[neighbor])
