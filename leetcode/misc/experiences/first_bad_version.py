14ebfa7c8162189f3f92d6432f2edc97cb3bda21
grewy <mgatuiet@gmail.com>
2020-05-16 16:27:45 +0530

    commit

---
+++ leetcode/misc/experiences/first_bad_version.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+
+        while l < r:
+            m = l + (r-l)//2
+
+            if isBadVersion(m):
+                r = m
+            else:
+                l = m + 1
+        return l
