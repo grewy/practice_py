cac5c4044f4ec67ad988432b77d316b6a6ac07d7
grewy <mgatuiet@gmail.com>
2020-05-23 23:52:18 +0530

    practice

--- leetcode/subsets/easy/subsets.py
+++ leetcode/subsets/easy/subsets.py
@@ -13,5 +13,35 @@ def subsets(arr):
         ans += [cur + [ch] for cur in ans]
     return ans
 
-print subsets([1,3,5])
-print subsets_bitmap([1,3,5])
+
+def subsetsWithDup(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = [[]]
+        for ch in sorted(nums):
+         ans += [cur + [ch] for cur in ans if cur + [ch] not in ans]
+        return ans
+
+def subsetsWithDup_1(nums):
+        """
+        Add if not added already - previous element is not same.
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return []
+        nums.sort()
+        res, cur = [[]], []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                cur = [item + [nums[i]] for item in cur]
+            else:
+                cur = [item + [nums[i]] for item in res]
+            res += cur
+        return res
+
+#print subsets([1,3,5])
+#print subsets_bitmap([1,3,5])
+print subsetsWithDup([4,4,4,1,4])
