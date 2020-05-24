9c3c3eb5198bcd01360593843ae58a50050cb37c
grewy <mgatuiet@gmail.com>
2020-05-25 01:24:51 +0530

    p

--- leetcode/BFS/medium/populating_next_pointer_ii.py
+++ leetcode/BFS/medium/populating_next_pointer_ii.py
@@ -1,5 +1,5 @@
 """
-# Definition for a Node.
+Definition for a Node.
 class Node(object):
     def __init__(self, val=0, left=None, right=None, next=None):
         self.val = val
@@ -17,14 +17,17 @@ class Solution(object):
         p= root
         while root:
             cur = tmp = Node(0)
+            #check current level
             while root:
                 if root.left:
+                    #tmp next linked to root.left
                     cur.next = root.left
                     cur = cur.next
                 if root.right:
                     cur.next = root.right
                     cur = cur.next
                 root = root.next
+            #move to next level
             root = tmp.next
         return p
 
