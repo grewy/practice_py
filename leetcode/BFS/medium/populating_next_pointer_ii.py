0fd0435c4553972b7bb308713718871c15469361
grewy <mgatuiet@gmail.com>
2020-05-25 01:20:43 +0530

    p

---
+++ leetcode/BFS/medium/populating_next_pointer_ii.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        p= root
+        while root:
+            cur = tmp = Node(0)
+            while root:
+                if root.left:
+                    cur.next = root.left
+                    cur = cur.next
+                if root.right:
+                    cur.next = root.right
+                    cur = cur.next
+                root = root.next
+            root = tmp.next
+        return p
+
