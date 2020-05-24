572b44aaf42e4d0169b81fcfbcfe63643686f76f
grewy <mgatuiet@gmail.com>
2020-05-25 00:23:14 +0530

    p

---
+++ leetcode/DFS/medium/kth_smallest_bst.py
@@ -0,0 +1,21 @@
+
+class Solution(object):
+
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop(-1)
+
+            k -= 1
+            if not k:
+                return root.val
+            root=root.right
