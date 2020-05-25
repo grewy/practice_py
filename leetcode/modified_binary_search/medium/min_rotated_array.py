dcf94aea8bc451906a44ef7efacf6b4045de6e41
grewy <mgatuiet@gmail.com>
2020-05-25 14:09:30 +0530

    p

--- leetcode/modified_binary_search/medium/min_rotated_array.py
+++ leetcode/modified_binary_search/medium/min_rotated_array.py
@@ -4,24 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #single element array
         if len(nums) == 1:
             return nums[0]
 
         l, r = 0, len(nums) - 1
-
+        #array is not rotated
         if nums[r] > nums[l]:
             return nums[l]
 
         while l <= r:
             m = l + (r-l)/2
 
+            # check if nums[m] is highest -> ans is next element
             if nums[m] > nums[m + 1]:
                 return nums[m + 1]
-
+            # check if nums[m] is lowest -> ans is nums[m]
             if nums[m-1] > nums[m]:
                 return nums[m]
-
-            if nums[m] > nums[0]:
+            # nums[l:m+1] is increasing part -> ans in nums[m+1:]
+            if nums[m] > nums[l]:
                 l = m + 1
             else:
                 r = m - 1
