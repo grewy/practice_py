270d11d71ad355b2073bc6aec48a71b58b13658a
grewy <mgatuiet@gmail.com>
2020-05-25 13:49:54 +0530

    p

--- leetcode/modified_binary_search/medium/closest_k_element.py
+++ leetcode/modified_binary_search/medium/closest_k_element.py
@@ -18,3 +18,27 @@ def closest_binary_search(arr, k, x):
         else:
             r = mid
     return a[l:l+k]
+
+
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        if x <= arr[0]:
+            return arr[:k]
+        elif x >= arr[-1]:
+            return arr[-k:]
+        else:
+            l, r = 0, len(arr) - k
+
+            while l < r:
+                m = l + (r-l)//2
+                if x - arr[m] > arr[m+k] - x:
+                    l = m+1
+                else:
+                    r = m
+            return arr[l:l+k]
