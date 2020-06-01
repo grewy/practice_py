9484dd4ba0316edc96764e590fcf07190a55ad1b
grewy <mgatuiet@gmail.com>
2020-06-02 00:47:11 +0530

    p

--- leetcode/DFS/easy/max_depth_binary_tree.py
+++ leetcode/DFS/easy/max_depth_binary_tree.py
@@ -30,3 +30,46 @@ class Solution(object):
 
         return max_depth
 
+
+i# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        queue = [root]
+        ans = 0
+        while queue:
+            nq = []
+            ans += 1
+            while queue:
+                root = queue.pop(0)
+
+                if root.left:
+                    nq.append(root.left)
+                if root.right:
+                    nq.append(root.right)
+            queue = nq
+
+
+        return ans
+
+
+def max_depth(root):
+    if not root:
+        return 0
+
+    lh = max_depth(root.left)
+    rh = max_depth(root.right)
+
+    return 1 + max(lh + rh)
