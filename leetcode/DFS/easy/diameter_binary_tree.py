ddc5c712f44144bdd97129461d78e7faf215cd2a
grewy <mgatuiet@gmail.com>
2020-06-02 00:31:37 +0530

    p

--- leetcode/DFS/easy/diameter_binary_tree.py
+++ leetcode/DFS/easy/diameter_binary_tree.py
@@ -24,4 +24,36 @@ class Solution(object):
         depth(root)
         return self.ans - 1
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        self.ans = float('-inf')
+
+        def height(root):
+            if not root:
+                return 0
+            lh = height(root.left)
+            rh = height(root.right)
+
+            # Diameter of tree is maximum value of (lheight + rheight +1 ) for each node
+            self.ans = max(self.ans, lh + rh + 1)
+            return 1 + max(lh, rh)
+
+
+        height(root)
+        return self.ans - 1
+
 
