a72bff8aaeccce57a610418098ed7a2823e7a133
grewy <mgatuiet@gmail.com>
2020-06-07 19:03:04 +0530

    p

---
+++ leetcode/data_structures/tree/traversals.py
@@ -0,0 +1,121 @@
+# Python program to for tree traversals
+
+# A class that represents an individual node in a
+# Binary Tree
+class Node:
+	def __init__(self,key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+
+# A function to do inorder tree traversal
+def printInorder(root):
+
+	if root:
+
+		# First recur on left child
+		printInorder(root.left)
+
+		# then print the data of node
+		print(root.val),
+
+		# now recur on right child
+		printInorder(root.right)
+
+
+def inorderIterative(root):
+    stack = []
+    while root or stack:
+
+        if  root:
+            stack.append(root)
+            root = root.left
+        else:
+            root = stack.pop()
+            print root.val,
+            root= root.right
+
+
+# A function to do postorder tree traversal
+def printPostorder(root):
+
+	if root:
+
+		# First recur on left child
+		printPostorder(root.left)
+
+		# the recur on right child
+		printPostorder(root.right)
+
+		# now print the data of node
+		print(root.val),
+
+
+def postorderIterative(root):
+
+    stack = [root]
+    out = []
+
+    while stack:
+        root = stack.pop()
+        out.append(root)
+
+        if root.left:
+            stack.append(root.left)
+
+        if root.right:
+            stack.append(root.right)
+
+    while out:
+        print out.pop().val,
+
+
+# A function to do preorder tree traversal
+def printPreorder(root):
+
+	if root:
+
+		# First print the data of node
+		print(root.val),
+
+		# Then recur on left child
+		printPreorder(root.left)
+
+		# Finally recur on right child
+		printPreorder(root.right)
+
+
+def preorderIterative(root):
+    stack = [root]
+
+    while stack:
+        root = stack.pop()
+        print root.val,
+
+        if root.right:
+            stack.append(root.right)
+        if root.left:
+            stack.append(root.left)
+
+
+# Driver code
+root = Node(1)
+root.left	 = Node(2)
+root.right	 = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+print "Preorder traversal of binary tree is"
+printPreorder(root)
+print
+preorderIterative(root)
+
+print "\nInorder traversal of binary tree is"
+printInorder(root)
+print
+inorderIterative(root)
+
+print "\nPostorder traversal of binary tree is"
+printPostorder(root)
+print
+postorderIterative(root)
