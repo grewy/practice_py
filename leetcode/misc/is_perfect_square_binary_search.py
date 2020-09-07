cab416a43f93dc4f4589ed0cacc4c474d3f5133e
grewy <mgatuiet@gmail.com>
2020-09-08 00:08:02 +0530

    practice

--- leetcode/misc/is_perfect_square_binary_search.py
+++ leetcode/misc/is_perfect_square_binary_search.py
@@ -14,3 +14,21 @@ class Solution(object):
             else:
                 l = m +1
         return False
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l, r = 0, x
+        while l <= r:
+            m = (l+r)//2
+            if m*m == x:
+                return m
+            if m*m > x:
+                r = m -1
+            else:
+                l = m+ 1
+        return r
