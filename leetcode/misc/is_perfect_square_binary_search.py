a916328d9ee6c0964a9c113105563353b84e6bb8
grewy <mgatuiet@gmail.com>
2020-09-08 00:07:20 +0530

    practice

---
+++ leetcode/misc/is_perfect_square_binary_search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        l, r = 0, num
+        while l <= r:
+            m = (l+r)//2
+            if m*m == num:
+                return True
+            elif m*m > num:
+                r = m -1
+            else:
+                l = m +1
+        return False
